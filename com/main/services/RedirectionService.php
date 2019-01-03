1c628704c841f08b45848b0bb6baf4bfa8c8900b
RaISug <radoslav.i.sugarev@gmail.com>
2019-01-03 23:06:43 +0200

    Fix redirection issue while adding products into cart

--- com/main/services/RedirectionService.php
+++ com/main/services/RedirectionService.php
@@ -10,12 +10,25 @@ class RedirectionService {
         $this->whitelistedPaths = array();
         
         $this->whitelistedPaths[] = "/\/products\/api\/v1\/type\/\w+?\/category\/[a-zA-Z ]+$/";
+        $this->whitelistedPaths[] = "/\/products\/api\/v1\/type\/\w+?\/category\/[a-zA-Z ]+\?insert=succeed$/";
+        
         $this->whitelistedPaths[] = "/\/products\/api\/v1\/category\/[a-zA-Z ]+$/";
+        $this->whitelistedPaths[] = "/\/products\/api\/v1\/category\/[a-zA-Z ]+\?insert=succeed$/";
+        
         $this->whitelistedPaths[] = "/\/products\/api\/v1\/type\/\w+$/";
+        $this->whitelistedPaths[] = "/\/products\/api\/v1\/type\/\w+\?insert=succeed$/";
+        
         $this->whitelistedPaths[] = "/\/products\/api\/v1$/";
+        $this->whitelistedPaths[] = "/\/products\/api\/v1\?insert=succeed$/";
+        
         $this->whitelistedPaths[] = "/\/products\/api\/v1\/collection\/[0-9a-zA-Z -]+$/";
+        $this->whitelistedPaths[] = "/\/products\/api\/v1\/collection\/[0-9a-zA-Z -]+\?insert=succeed$/";
+        
         $this->whitelistedPaths[] = "/\/products\/api\/v1\/product\/[0-9]+$/";
+        $this->whitelistedPaths[] = "/\/products\/api\/v1\/product\/[0-9]+\?insert=succeed$/";
+        
         $this->whitelistedPaths[] = "/\/carts\/api\/v1$/";
+        $this->whitelistedPaths[] = "/\/carts\/api\/v1\?insert=succeed$/";
     }
 
     public function constructRedirectionPath() {    
@@ -25,11 +38,19 @@ class RedirectionService {
 
         foreach ($this->whitelistedPaths as $whitelistedPath) {
             if (preg_match($whitelistedPath, $referrer, $matches) === 1) {
+                if ($this->endsWith($matches[0], "?insert=succeed")) {
+                    return $matches[0];
+                }
+
                 return $matches[0] . "?insert=succeed";
             }
         }
         
         return "/products/api/v1?insert=succeed";
+    }
+
+    function endsWith($haystack, $needlee) {
+        return (substr($haystack, -strlen($needlee)) === $needlee);
     }
 
 }
